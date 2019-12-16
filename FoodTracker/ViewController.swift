1dd021e645a7788ef6c84180ade0582e8effb5c0
xpy0035 <cristian.recalde.contractor@bbva.com>
2019-12-16 09:28:45 -0300

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Cristhian Jesus Recalde Franco on 12/16/19.
+//  Copyright © 2019 Cristhian Jesus Recalde Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
